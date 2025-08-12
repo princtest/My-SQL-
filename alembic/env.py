e2c7c27773ee14871ff85ef408984ef1ae7aaf97
princtest <princemelgandi1112@gmail.com>
2025-08-12 20:55:54 +0530

    Update env.py

--- alembic/env.py
+++ alembic/env.py
@@ -1,1 +1,46 @@
+import os
+from logging.config import fileConfig
+from alembic import context
+from sqlalchemy import engine_from_config, pool
 
+# this is the Alembic Config object, which provides
+# access to the values within the .ini file in use.
+config = context.config
+
+# Interpret the config file for Python logging.
+fileConfig(config.config_file_name)
+
+# Import your app's metadata object
+from app.database import Base  # noqa
+target_metadata = Base.metadata
+
+def get_url():
+    return os.getenv("DATABASE_URL", "sqlite:///./dev.db")
+
+config.set_main_option("sqlalchemy.url", get_url())
+
+def run_migrations_offline():
+    url = config.get_main_option("sqlalchemy.url")
+    context.configure(
+        url=url,
+        target_metadata=target_metadata,
+        literal_binds=True,
+    )
+    with context.begin_transaction():
+        context.run_migrations()
+
+def run_migrations_online():
+    connectable = engine_from_config(
+        config.get_section(config.config_ini_section),
+        prefix='sqlalchemy.',
+        poolclass=pool.NullPool,
+    )
+    with connectable.connect() as connection:
+        context.configure(connection=connection, target_metadata=target_metadata)
+        with context.begin_transaction():
+            context.run_migrations()
+
+if context.is_offline_mode():
+    run_migrations_offline()
+else:
+    run_migrations_online()
