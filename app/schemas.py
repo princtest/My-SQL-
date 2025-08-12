0a634a2e66cd5ce104b61b33ff95b9b0e95c3dbd
princtest <princemelgandi1112@gmail.com>
2025-08-12 21:00:04 +0530

    Create schemas.py

---
+++ app/schemas.py
@@ -0,0 +1,30 @@
+from pydantic import BaseModel, EmailStr
+from typing import Optional, List
+
+class ItemBase(BaseModel):
+    title: str
+    description: Optional[str] = None
+
+class ItemCreate(ItemBase):
+    pass
+
+class Item(ItemBase):
+    id: int
+    owner_id: int
+
+    class Config:
+        orm_mode = True
+
+class UserBase(BaseModel):
+    email: EmailStr
+
+class UserCreate(UserBase):
+    password: str
+
+class User(UserBase):
+    id: int
+    is_active: bool
+    items: List[Item] = []
+
+    class Config:
+        orm_mode = True
